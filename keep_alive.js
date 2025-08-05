15e6e3e84f543346991c56f7bedb06f32b395b89
akhil5559 <2akhil257@gmail.com>
2025-08-05 15:22:23 +0530

    Create keep_alive.js

---
+++ keep_alive.js
@@ -0,0 +1,8 @@
+import http from 'http';
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is alive!');
+}).listen(process.env.PORT || 3000);
+
+console.log('Keep-alive server running...');
